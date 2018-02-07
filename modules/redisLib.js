0d80d59b13a05b12a27ae1d1d464fa96c07028fd
Fadi Alladkani <fadi.alladkani@outlook.com>
2018-02-07 08:33:20 +0200

    added redisLib method skeletons

---
+++ modules/redisLib.js
@@ -0,0 +1,5 @@
+//A collection of methods for reading and writing redis files
+exports.getAll = () => {}; //TODO: Add method
+exports.getIf = () => {}; //TODO: Add method
+exports.getFile = () => {}; //TODO: Add method
+exports.setFile = () => {}; //TODO: Add method
