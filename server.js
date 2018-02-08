ce41732ef6c3f1c91c21379cc471683e5e088b58
Fadi Alladkani <fadi.alladkani@outlook.com>
2018-02-08 13:29:47 +0200

    added socket functions

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var http = require('http');
 var path = require('path');
 var log = require('morgan');
 var redis = require('redis');
+var socket = require('socket.io');
 
 var app = express();
 var client = redis.createClient();
@@ -16,7 +17,25 @@ else if(process.env.NODE_ENV == 'production')
   app.use(express.static(path.join(__dirname, 'client/build')));
 
 var server = http.createServer(app);
+var io = socket(server);
 server.listen(PORT);
+
+//Socket Config
+
+io.on('connection', function(socket){
+  socket.on('files', function(){
+    console.log('Socket requested list of files');
+  });
+  socket.on('fileReq', function(id, msg){
+    console.log('Socket requested ' + msg);
+  });
+  socket.on('keystroke', function(id, msg){
+    console.log('Socket typed ' + msg);
+  });
+  socket.on('fileClose', function(id, msg){
+    console.log('Socket closed file');
+  });
+});
 
 //Other Functions
 
