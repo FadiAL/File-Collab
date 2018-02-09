13ea3f52aeff5aa157bbf3497b7df5c7fc12cdfd
Fadi Alladkani <fadi.alladkani@outlook.com>
2018-02-09 13:06:43 +0200

    setup webpack imports

--- client/dev/javascripts/cSocket.js
+++ client/dev/javascripts/cSocket.js
@@ -1,3 +1,7 @@
+import io from 'socket.io-client';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
 var socket = io();
 socket.emit('files', '');
 socket.on('files', function(fileList){
