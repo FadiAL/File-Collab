e306361debc5f0383518a8043c4cfbe72d76b701
Fadi Alladkani <fadi.alladkani@outlook.com>
2018-02-11 18:27:23 +0200

    FileList now keeps one active elem

--- client/dev/javascripts/cSocket.js
+++ client/dev/javascripts/cSocket.js
@@ -16,21 +16,33 @@ socket.on('files', function(fileList){
 class File extends React.Component {
   constructor(props){
     super(props);
-    this.state = {active: false};
+  }
+  fileSelected(e) {
+    if(!this.props.active)
+      this.props.onFileSelected(this.props.name);
   }
   render() {
-    return <li className={"pure-menu-item" + (this.state.active ? "pure-menu-selected" : "")}>
-    <a className="pure-menu-link" onClick={fileSelected}>{this.props.name}</a>
+    return <li className={"pure-menu-item " + (this.props.active ? "pure-menu-selected" : "")}>
+    <a className="pure-menu-link" onClick={(e) => this.fileSelected(e)}>{this.props.name}</a>
     </li>
   }
 }
-function fileSelected() {
-  //TODO:Add function
-}
+
 class FileList extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {activeFile: ''}
+  }
+  handleFileChange(e) {
+    this.setState({activeFile: e});
+  }
   render() {
     const files = this.props.files;
-    const list = files.map(file => <File name={file} />);
+    const list = files.map(
+      file => <File
+       name={file} onFileSelected={(e) => this.handleFileChange(e)}
+       active={file == this.state.activeFile}
+      />);
     return (
       <div className="pure-menu">
         <a className="pure-menu-heading">Files</a>
