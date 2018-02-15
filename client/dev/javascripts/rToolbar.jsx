4b012878e9f6644fb986076cd57e8dd0d6d4e119
Fadi Alladkani <fadi.alladkani@outlook.com>
2018-02-15 19:07:28 +0200

    added basic class

---
+++ client/dev/javascripts/rToolbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Toolbar extends React.Component {
+  render() {
+    return (
+      <div id="toolbar">
+        <img id="add-file" src="icons/plus.png" />
+        <img id="remove-file" src="icons/delete.png" />
+      </div>
+    )
+  }
+}
