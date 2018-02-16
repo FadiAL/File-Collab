dc4279ceac1f25db56bf9a0c73bde372b01ac8e1
Fadi Alladkani <fadi.alladkani@outlook.com>
2018-02-16 15:35:36 +0200

    more components integrated

--- client/dev/javascripts/rMainPanel.jsx
+++ client/dev/javascripts/rMainPanel.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import Content from './rContent.jsx';
 import Toolbar from './rToolbar.jsx';
+import FileList from './rFile.jsx';
 
 class MainPanel extends React.Component {
   render() {
     return (
-      <div className="content">
-        <Toolbar fileAdd={this.props.fileAdd}/>
-        <br/>
-        <Content text=""/>
+      <div id="layout">
+        <div id="menu">
+          <div className="pure-menu" id="file-list">
+            <FileList socket={this.props.socket}/>
+          </div>
+        </div>
+        <div className="content">
+          <Toolbar fileAdd={this.props.fileAdd}/>
+          <br/>
+        </div>
       </div>
     )
   }
