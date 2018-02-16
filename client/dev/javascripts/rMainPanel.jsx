7b187d12c9b32ca7bf9f0d4686549c948a644f0d
Fadi Alladkani <fadi.alladkani@outlook.com>
2018-02-16 18:48:22 +0200

    added state rendering and socket

--- client/dev/javascripts/rMainPanel.jsx
+++ client/dev/javascripts/rMainPanel.jsx
@@ -4,17 +4,37 @@ import Toolbar from './rToolbar.jsx';
 import FileList from './rFile.jsx';
 
 class MainPanel extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {curFile: null};
+    this.setupSocket(this.props.socket);
+  }
+  setupSocket(socket) {
+    socket.on('fileReq', file => {
+      this.setState({curFile: file});
+    });
+  }
+  onFileSelected(fileName) {
+    this.setState({curFileName: fileName});
+  }
+  handleDelete() {
+    this.props.fileDelete(this.state.curFileName);
+  }
   render() {
     return (
       <div id="layout">
         <div id="menu">
           <div className="pure-menu" id="file-list">
-            <FileList socket={this.props.socket}/>
+            <FileList socket={this.props.socket} onFileChange={() => {this.onFileSelected}}/>
           </div>
         </div>
         <div className="content">
-          <Toolbar fileAdd={this.props.fileAdd}/>
+          <Toolbar fileAdd={this.props.fileAdd}
+                    fileDelete={(this.state.curFile || this.state.curFile === "") ? this.handleDelete : undefined}/>
           <br/>
+          {this.state.curFile &&
+            <Content text={this.state.curFile}/>
+          }
         </div>
       </div>
     )
