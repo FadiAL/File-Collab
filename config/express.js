4ec5fd17fc230494ddce3ea4484170c19fb54e5d
Fadi Alladkani <fadi.alladkani@outlook.com>
2018-02-23 17:44:37 +0200

    fixed issues

--- config/express.js
+++ config/express.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const morgan = require('morgan');
 
+var rootPath = process.env.NODE_ENV == 'production' ? 'client/build' : 'client/dev';
+
 module.exports = function(app) {
-  app.use(morgan(tiny));
-  app.use(express.static(root));
+  app.use(morgan('tiny'));
+  app.use(express.static(rootPath));
 }
--- server.js
+++ server.js
@@ -13,53 +13,12 @@ var rooms = {};
 
 const PORT = process.env.PORT || 8080;
 
-app.use(log('tiny'));
-
-if(process.env.NODE_ENV == 'production')
-  app.use(express.static(path.join(__dirname, 'client/build')));
-else
-  app.use(express.static(path.join(__dirname, 'client/dev')));
 var server = http.createServer(app);
-var io = socket(server);
+
+require('./config/express.js')(app);
+require('./config/socket.js')(server, socket);
+
 server.listen(PORT);
-
-//Socket Config
-
-io.on('connection', function(socket){
-  socket.on('files', function(){
-    redisLib.getAll(function(val){
-      socket.emit('files', val);
-    });
-  });
-  socket.on('fileReq', function(msg){
-    redisLib.getFile(msg, function(val){
-      socket.join(msg);
-      rooms[socket.id] = msg;
-      socket.emit('fileReq', val);
-    });
-  });
-  socket.on('keystroke', function(msg){
-    socket.broadcast.to(rooms[socket.id]).emit('keystroke', msg);
-    redisLib.update(rooms[socket.id], msg);
-  });
-  socket.on('fileCreate', function(fileName){
-    redisLib.create(fileName, function(){
-      socket.emit('fileTaken', fileName);//Function if name is taken
-    }, function(){
-      io.sockets.emit('fileCreate', fileName);//Function if create works
-      socket.emit('currentFileCreated', fileName);//Function to send to user that created the file
-    });
-  });
-  socket.on('fileClose', function(id, msg){
-    console.log('Socket closed file');
-  });
-  socket.on('delete', function(key){
-    redisLib.delete(key, function(){
-      io.sockets.emit('delete', key);
-      io.to(rooms[socket.id]).emit('deletedOpen');
-    });
-  });
-});
 
 //Other Functions
 
