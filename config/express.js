8300002e4e900087a3e3ee9aaa3bd8e377ac7481
Fadi Alladkani <fadi.alladkani@outlook.com>
2018-02-24 11:40:43 +0200

    added compression

--- config/express.js
+++ config/express.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const morgan = require('morgan');
+const compression = require('compression');
 
 var rootPath = process.env.NODE_ENV == 'production' ? 'client/build' : 'client/dev';
 
 module.exports = function(app) {
   app.use(morgan('tiny'));
+  app.use(compression());
   app.use(express.static(rootPath));
 }
