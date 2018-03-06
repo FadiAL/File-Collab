f5686db88d3a9358c7f64c5b1a977c8b6712933a
Fadi Alladkani <fadi.alladkani@outlook.com>
2018-03-06 11:49:25 +0200

    added action types

---
+++ client/dev/javascripts/redux/actionTypes.js
@@ -0,0 +1,6 @@
+//action types
+export const SET_FILE_NAME = "SET_FILE_NAME";
+export const SET_FILE_CONTENTS = "SET_FILE_CONTENTS";
+export const UPDATE_RECENTS = "UPDATE_RECENTS";
+export const UPDATE_FILES = "UPDATE_FILES";
+export const SET_FILE_OPEN = "SET_FILE_OPEN";
