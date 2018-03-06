ebfb38817b912e2f6a4bbaa755fcc5b92620f0d7
Fadi Alladkani <fadi.alladkani@outlook.com>
2018-03-06 12:06:42 +0200

    added basic reducer

---
+++ client/dev/javascripts/redux/reducers.js
@@ -0,0 +1,12 @@
+const initialState = {
+  file {
+    fileName: null,
+    fileContents: null,
+    fileOpen: false
+  }
+  recents: [],
+  files: []
+}
+function fileApp (state = initialState, action) {
+  return state;
+}
