befcea9727ea003d3e8f055cf04347388487e89d
Fadi Alladkani <fadi.alladkani@outlook.com>
2018-03-06 11:57:31 +0200

    added action creators

--- client/dev/javascripts/redux/actionTypes.js
+++ client/dev/javascripts/redux/actionTypes.js
@@ -4,3 +4,20 @@ export const SET_FILE_CONTENTS = "SET_FILE_CONTENTS";
 export const UPDATE_RECENTS = "UPDATE_RECENTS";
 export const UPDATE_FILES = "UPDATE_FILES";
 export const SET_FILE_OPEN = "SET_FILE_OPEN";
+
+//action creators
+export function changeFileName (newName){
+  return {type: SET_FILE_NAME, newName}
+}
+export function changeFileContents (newContents) {
+  return {type: SET_FILE_CONTENTS, newContents}
+}
+export function updateRecents (newList) {
+  return {type: UPDATE_RECENTS, newList}
+}
+export function updateFiles (newFiles) {
+  return {type: UPDATE_FILES, newFiles}
+}
+export function toggleFileOpen () {
+  return {type: SET_FILE_OPEN}
+}
