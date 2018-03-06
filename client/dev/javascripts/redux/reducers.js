fde44ac50ea227e0f5df00aabd0912de4c7fe318
Fadi Alladkani <fadi.alladkani@outlook.com>
2018-03-06 16:16:33 +0200

    added action handling

--- client/dev/javascripts/redux/reducers.js
+++ client/dev/javascripts/redux/reducers.js
@@ -1,5 +1,13 @@
+import {
+  SET_FILE_NAME,
+  SET_FILE_OPEN,
+  SET_FILE_CONTENTS,
+  UPDATE_RECENTS,
+  UPDATE_FILES
+} from './actions.js'
+
 const initialState = {
-  file {
+  file: {
     fileName: null,
     fileContents: null,
     fileOpen: false
@@ -7,6 +15,34 @@ const initialState = {
   recents: [],
   files: []
 }
+
 function fileApp (state = initialState, action) {
+  switch (action.type) {
+    case SET_FILE_NAME:
+      Object.assign({}, state, Object.assign({}, state.file, {
+        fileName: action.newName;
+      }));
+      break;
+    case SET_FILE_CONTENTS:
+      Object.assign({}, state, Object.assign({}, state.file, {
+        fileContents: action.newContents;
+      }));
+      break;
+    case SET_FILE_OPEN:
+      Object.assign({}, state, Object.assign({}, state.file, {
+        fileOpen: !state.file.fileOpen;
+      }));
+      break;
+    case UPDATE_RECENTS:
+      Object.assign({}, state, {
+        recents: action.newList;
+      });
+      break;
+    case UPDATE_FILES:
+      Object.assign({}, state, {
+        files: action.newFiles;
+      });
+      break;
+  }
   return state;
 }
