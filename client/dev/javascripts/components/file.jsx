ac9ca44497b1fca59a2380cf8b7859800a3f1c14
Fadi Alladkani <fadi.alladkani@outlook.com>
2018-03-29 23:56:16 +0300

    added basic file elem

---
+++ client/dev/javascripts/components/file.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class File extends React.Component {
+  render() {
+    return (
+      <li className={"pure-menu-item" + (this.props.active ? "pure-menu-selected" : "")}>
+        <a className="pure-menu-link" onClick={(e) => this.props.onFileSelected(e)}>
+          {this.props.name}
+        </a>
+      </li>
+    )
+  }
+}
