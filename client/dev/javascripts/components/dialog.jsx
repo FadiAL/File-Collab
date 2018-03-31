a3d5047d0d60c445a64baeab8a4130edd777abf1
Fadi Alladkani <fadi.alladkani@outlook.com>
2018-03-31 22:38:14 +0300

    added basic dialog

---
+++ client/dev/javascripts/components/dialog.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Dialog extends React.Component {
+  render() {
+    return (
+      <form className="pure-form pure-form-stacked" onSubmit={this.props.createFile}>
+        <h2>Create File</h2>
+        {this.props.error &&
+          <h5>Error, file name {this.props.error} taken</h5>
+        }
+        <input type="text" placeHolder="File Name" ref={input => {this.fileInput=input;}}/>
+        <button type="submit" className="pure-button pure-button-primary">Create</button>
+      </form>
+    )
+  }
+}
