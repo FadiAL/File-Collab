4393a3352355ff8d31eeb7287bd678b2ee5d7895
Fadi Alladkani <fadi.alladkani@outlook.com>
2018-03-31 23:51:04 +0300

    added listeners file

---
+++ client/dev/javascripts/redux/listeners.js
@@ -0,0 +1,20 @@
+import io from 'socket.io-client';
+import {
+  recieveFiles,
+  recieveFile,
+  recieveRecents
+} from './actions.js';
+
+export function setupSocket(store) {
+  const socket = io();
+
+  socket.on('files', files => {
+    store.dispatch(recieveFiles(files));
+  });
+  socket.on('fileReq', file => {
+    store.dispatch(recieveFile(file));
+  });
+  socket.on('recentFiles', recents => {
+    store.dispatch(recieveRecents(recents));
+  });
+}
