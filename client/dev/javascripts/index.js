c361108993a323e5a92dfcf63fbf8c5be8ba0117
Fadi Alladkani <fadi.alladkani@outlook.com>
2018-04-23 22:26:09 +0300

    fixed rendering

--- client/dev/javascripts/index.js
+++ client/dev/javascripts/index.js
@@ -1,12 +1,13 @@
-import io from 'socket.io-client';
 import React from 'react';
 import {render} from 'react-dom';
 import {Provider} from 'react-redux';
 import {createStore} from 'redux';
-import {fileApp} from './redux/reducers.js';
+import fileApp from './redux/reducers.js';
+import {setupSocket} from './redux/listeners.js';
+import App from './components/App.jsx';
 
-const socket = io();
 const store = createStore(fileApp);
+const socket = setupSocket(store);
 
 render(
   <Provider store={store}>
