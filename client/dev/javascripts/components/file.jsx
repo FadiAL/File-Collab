f8e725883ebed332ecb477f1087920388bdf75f4
Fadi Alladkani <fadi.alladkani@outlook.com>
2018-04-26 09:06:26 +0300

    added file onClick

--- client/dev/javascripts/components/file.jsx
+++ client/dev/javascripts/components/file.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 class File extends React.Component {
   render() {
     return (
-      <li className={"pure-menu-item" + (this.props.active ? "pure-menu-selected" : "")}>
-        <a className="pure-menu-link" onClick={(e) => this.props.onFileSelected(e)}>
+      <li className={"pure-menu-item" + (this.props.active ? "pure-menu-selected" : "")} onClick={this.props.onClick}>
+        <a className="pure-menu-link">
           {this.props.name}
         </a>
       </li>
