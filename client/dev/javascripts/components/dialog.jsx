c2e8ffb440e84c790b405a56ba8b85e0ed1ef4c6
Fadi Alladkani <fadi.alladkani@outlook.com>
2018-05-02 01:14:07 +0300

    added dialog

--- client/dev/javascripts/components/dialog.jsx
+++ client/dev/javascripts/components/dialog.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {connect} from 'react-redux';
 
 class Dialog extends React.Component {
   render() {
@@ -14,3 +15,5 @@ class Dialog extends React.Component {
     )
   }
 }
+
+export default Dialog;
