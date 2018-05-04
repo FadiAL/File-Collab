be38a3a429b4216b826b6330a0777ab7c7a99cd8
Fadi Alladkani <fadi.alladkani@outlook.com>
2018-05-04 10:02:46 +0300

    improved and fixed toolbar

--- client/dev/javascripts/components/toolbar.jsx
+++ client/dev/javascripts/components/toolbar.jsx
@@ -1,27 +1,37 @@
 import React from 'react';
 import {connect} from 'react-redux';
+import {requestFiles, toggleCreatingFile} from '../redux/actions.js';
 
 class Toolbar extends React.Component {
   render() {
     return (
       <div id="toolbar">
-        <img id="add-file" src="icons/plus.png" onClick={() => this.props.addFile()}/>
-        {this.props.fileOpen &&
-          <img id="remove-file" src="icons/delete.png" onClick={() => this.props.deleteFile()}/>
-        }
-        {this.props.fileOpen &&
-          <img id="remove-file" src="icons/back.png" onClick={() => this.props.leaveFile()}/>
-        }
+        <div id="toolbarFilename">
+          {this.props.fileName}
+        </div>
+        <div id="buttons">
+          <img id="add-file" src="icons/plus.png" onClick={() => this.props.addFile()}/>
+          {this.props.fileOpen &&
+            <img id="remove-file" src="icons/delete.png" onClick={() => this.props.deleteFile()}/>
+          }
+          {this.props.fileOpen &&
+            <img id="remove-file" src="icons/back.png" onClick={() => this.props.leaveFile()}/>
+          }
+        </div>
       </div>
     )
   }
 }
 
 const mapStateToProps = state => ({
-  fileOpen: state.fileOpen
+  fileOpen: state.fileOpen,
+  fileName: state.file.name
 });
-
+const mapDispatchToProps = dispatch => ({
+  leaveFile: () => dispatch(requestFiles()),
+  addFile: () => dispatch(toggleCreatingFile())
+})
 export default connect(
   mapStateToProps,
-  null
+  mapDispatchToProps
 )(Toolbar);
