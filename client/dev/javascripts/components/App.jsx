1dd4269fe693a8c7e6baa3bb83fd905cd28b64cf
Fadi Alladkani <fadi.alladkani@outlook.com>
2018-05-04 10:02:03 +0300

    added dialog to App

--- client/dev/javascripts/components/App.jsx
+++ client/dev/javascripts/components/App.jsx
@@ -2,24 +2,51 @@ import React from 'react';
 import Toolbar from './toolbar.jsx';
 import Recents from './recents.jsx';
 import MainPanel from './mainPanel.jsx';
+import Dialog from './dialog.jsx';
+import {connect} from 'react-redux';
+
+import {toggleCreatingFile} from '../redux/actions.js';
 
 class App extends React.Component {
   render() {
+    const creatingFile = this.props.creatingFile;
+    const onCancel = () => {
+      if(creatingFile)
+        this.props.onCancelCreate();
+      else
+        return;
+    };
     return (
-      <div id="layout">
-        <div id="menu">
-          <div className="pure-menu" id="file-list">
-            <Recents/>
+      <div id="main">
+        <div id="layout" className={creatingFile ? "blur" : ""} onClick={() => onCancel()}>
+          <div id="menu">
+            <div className="pure-menu" id="file-list">
+              <Recents/>
+            </div>
+          </div>
+          <div className="content">
+            <br/>
+            <MainPanel/>
+            <Toolbar/>
           </div>
         </div>
-        <div className="content">
-          <br/>
-          <MainPanel/>
-          <Toolbar/>
-        </div>
+        {creatingFile &&
+          <div id="create-dialog">
+            <Dialog/>
+          </div>
+        }
       </div>
     )
   }
 }
+const mapStateToProps = state => ({
+  creatingFile: state.creatingFile
+});
+const mapDispatchToProps = dispatch => ({
+  onCancelCreate: () => dispatch(toggleCreatingFile())
+});
 
-export default App;
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
