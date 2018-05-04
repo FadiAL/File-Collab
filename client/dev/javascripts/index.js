cd343c4fac3ebb3f11b7f332d0e64c06f5f522f8
Fadi Alladkani <fadi.alladkani@outlook.com>
2018-05-04 10:03:37 +0300

    updated index with middleware

--- client/dev/javascripts/index.js
+++ client/dev/javascripts/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import {render} from 'react-dom';
 import {Provider} from 'react-redux';
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
 import fileApp from './redux/reducers.js';
-import {setupSocket} from './redux/listeners.js';
+import {setupSocket} from './socket/listeners.js';
 import App from './components/App.jsx';
+import socketMiddlewareCreator from './socket/socketMiddleware.js';
+import io from 'socket.io-client';
 
-const store = createStore(fileApp);
-const socket = setupSocket(store);
+const socket = io();
+
+const sMiddleware = socketMiddlewareCreator(socket);
+
+const store = createStore(fileApp, applyMiddleware(sMiddleware));
+
+setupSocket(socket, store);
 
 render(
   <Provider store={store}>
